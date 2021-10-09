87018be0164da02a74c1e7673f88769c6fb402ea
陈裕聪 <yucong.chen@msxf.com>
2021-10-09 14:39:16 +0800

    提交framework

---
+++ IvyFramework.framework/Headers/Ivy.h
@@ -0,0 +1,17 @@
+//
+//  Ivy.h
+//  IvyFramework
+//
+//  Created by yuchong.chen on 2021/10/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Ivy : NSObject
++(void)eat;
++(void)sleep;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ IvyFramework.framework/Headers/IvyFramework.h
@@ -0,0 +1,18 @@
+//
+//  IvyFramework.h
+//  IvyFramework
+//
+//  Created by yuchong.chen on 2021/10/9.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IvyFramework.
+FOUNDATION_EXPORT double IvyFrameworkVersionNumber;
+
+//! Project version string for IvyFramework.
+FOUNDATION_EXPORT const unsigned char IvyFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IvyFramework/PublicHeader.h>
+
+
